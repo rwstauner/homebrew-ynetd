839acb32f302f408a07029ad834747111c448738
Randy Stauner <randy@r4s6.net>
2019-10-05 15:25:31 -0700

    Upgrade to v0.13 and use GO111MODULE to simplify installation

--- Formula/ynetd.rb
+++ Formula/ynetd.rb
@@ -1,31 +1,17 @@
-require "language/go"
+# rubocop: disable all
 
 class Ynetd < Formula
-  desc "A tiny super server written in go"
+  desc "micromanage your microservices with a tiny super server written in go"
   homepage "https://github.com/rwstauner/ynetd"
-  url "https://github.com/rwstauner/ynetd.git", :tag => "v0.11"
+  url "https://github.com/rwstauner/ynetd.git", :tag => "v0.13"
   head "https://github.com/rwstauner/ynetd.git", :branch => "master"
 
   depends_on "go" => :build
 
-  go_resource "github.com/hashicorp/go-reap" do
-    url "https://github.com/hashicorp/go-reap.git"
-  end
-
-  go_resource "gopkg.in/yaml.v2" do
-    url "https://gopkg.in/yaml.v2.git"
-  end
-
-  go_resource "golang.org/x/sys" do
-    url "https://go.googlesource.com/sys.git"
-  end
-
   def install
-    ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/rwstauner/ynetd"
-    dir.install buildpath.children
-    Language::Go.stage_deps resources, buildpath/"src"
-    cd dir do
+    ENV.delete("GOPATH")
+    ENV["GO111MODULE"] = "on"
+    cd buildpath do
       system "go", "build", "-ldflags", "-X main.Version=v#{version}", "-o", bin/"ynetd"
     end
   end
